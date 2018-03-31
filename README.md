cdcc1c17f1f18d0525135c153cc6c64882553aa2
Talin <yintaoxue@gmail.com>
2018-04-01 01:13:00 +0800

    dev schema

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SenseML
 Machine Learning Framework with feature generation, selection, training and evaluation
+
+## Feature
+
+
+#### TODO List
+* Feature Generator
+* Feature Evaluator
+* Feature Selection
+* Streaming Feature
+* ...
--- feature/src/test/java/org/senseml/AppTest.java
+++ feature/src/test/java/org/senseml/AppTest.java
@@ -1,32 +1,29 @@
 package org.senseml;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+public class AppTest {
+
+
+    @Test
+    public void testMain() {
+        System.out.println("start test SenseML main!!!");
+
+        // rawdata
+
+
+
+
+
+        System.out.println("end test SenseML main!!!");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
+    @Test
     public void testDateSet() {
         List<String> orders = DateSet.getOrdersData();
         System.out.println("orders:");
